8a07714f9f933181654d6d465fb6d1720f30eaf6
denis_kraken_kubuntu <amakiller229@gmail.com>
2025-08-28 13:19:06 +0300

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
     images: {
         domains: ['images.unsplash.com','avatars.mds.yandex.net','www.google.com','frankfurt.apollo.olxcdn.com'],
     },
